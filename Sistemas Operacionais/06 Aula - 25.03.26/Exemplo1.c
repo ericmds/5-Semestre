14d0bfeea2a7158a2e39cb2cb3bd7d2526e75ca0
Éric <deck.martins.silva@gmail.com>
2026-04-01 08:54:48 -0300

    Update Exemplo1.c

--- Sistemas Operacionais/06 Aula - 25.03.26/Exemplo1.c
+++ Sistemas Operacionais/06 Aula - 25.03.26/Exemplo1.c
@@ -40,27 +40,3 @@ int main (int argc, char *argv[])
 	printf ("%ld: Bye bye World!\n", (long) threadid);
 	pthread_exit (NULL); // encerra esta thread
 }
-
-// thread "main"
-int main (int argc, char *argv[])
-{
-	pthread_t thread[NUM_THREADS];
-	long status, i;
-
-	// cria as demais threads
-	for(i = 0; i < NUM_THREADS; i++)
-	{
-		printf ("Creating thread %ld\n", i);
-		status = pthread_create (&thread[i], NULL, print_hello, (void *) i);
-		pthread_join(thread[i], NULL);
-
-		if (status) // ocorreu um erro na criação
-		{
-			perror ("pthread_create");
-			exit (-1);
-		}
-	}
-
-	// encerra a thread "main"
-	pthread_exit (NULL);
-}
