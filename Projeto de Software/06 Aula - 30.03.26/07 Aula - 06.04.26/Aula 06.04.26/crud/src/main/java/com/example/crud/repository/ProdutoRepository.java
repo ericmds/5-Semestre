f74b8cffa1982d8fc8d1e1076ca8f5756225f215
Éric <deck.martins.silva@gmail.com>
2026-04-06 10:57:29 -0300

    Delete Projeto de Software/06 Aula - 30.03.26/07 Aula - 06.04.26/Aula 06.04.26/crud directory

--- Projeto de Software/06 Aula - 30.03.26/07 Aula - 06.04.26/Aula 06.04.26/crud/src/main/java/com/example/crud/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud.repository;
-
-import com.example.crud.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
