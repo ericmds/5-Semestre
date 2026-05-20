1ba3732e2b01046e36ddad998d0dd6e243e4b493
Éric <deck.martins.silva@gmail.com>
2026-05-20 09:20:01 -0300

    Create Recurso.java

---
+++ Sistemas Operacionais/14 Aula - 20.05.26/Recurso.java
@@ -0,0 +1,23 @@
+package DeadLock;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Recurso {
+    
+    private String nome;
+
+    public Recurso(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
